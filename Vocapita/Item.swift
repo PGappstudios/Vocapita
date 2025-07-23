8575ee74831e6fb2a683e4866fbc990b793bb4e8
pg <pgmetastudios@gmail.com>
2025-07-23 15:21:06 +0100

    Initial Commit

---
+++ Vocapita/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Vocapita
+//
+//  Created by Pedro Gregorio on 23/07/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
